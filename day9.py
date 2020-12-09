8adb6bb2277be2a657059d927db6db3c1e080355
Nikola Kasev <nikola.kasev@gmail.com>
2020-12-09 10:21:48 +0100

    solved D9P2

--- day9.py
+++ day9.py
@@ -21,3 +21,18 @@ def check(numbers, preamble):
 
 
 print(check(a, preamble))
+
+
+def contiguous_list(numbers, invalid_number):
+    latest_range = []
+    for i in range(len(numbers)):
+        for j in range(i, len(numbers)):
+            subset = numbers[i:j+1]
+            if sum(subset) == invalid_number and len(latest_range) < len(subset):
+                latest_range = subset
+
+    return latest_range
+
+
+c = contiguous_list(a, check(a, preamble))
+print(min(c) + max(c))
