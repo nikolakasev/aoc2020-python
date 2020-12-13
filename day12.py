c17f674daa05ea718210ad356738f766c57875df
Nikola Kasev <nikola.kasev@gmail.com>
2020-12-13 22:15:12 +0100

    solved D12P2

--- day12.py
+++ day12.py
@@ -96,5 +96,51 @@ for i in range(len(a)):
         print(f"don't know instruction {op} {num}")
 
 
-print(x, y, orientation)
 print(x + y)
+
+# East and South go plus, North and West go negative
+waypoint = (10, -1)
+
+
+def move_waypoint(w, direction, num):
+    if direction == 'E':
+        return (w[0] + num, w[1])
+    elif direction == 'W':
+        return (w[0] - num, w[1])
+    elif direction == 'N':
+        return (w[0], w[1] - num)
+    else:
+        # must be South
+        return (w[0], w[1] + num)
+
+
+def move_to_waypoint(x, y, w, times):
+    return (x + w[0] * times, y + w[1] * times)
+
+
+def rotate_waypoint(w, direction, degrees):
+    if (direction == 'R' and degrees == 90) or (direction == 'L' and degrees == 270):
+        return (-w[1], w[0])
+    elif (direction == 'R' and degrees == 180) or (direction == 'L' and degrees == 180):
+        return (-w[0], -w[1])
+    else:
+        # R 270 or L 90
+        return (w[1], -w[0])
+
+
+x = 0
+y = 0
+for i in range(len(a)):
+    op = a[i][:1]
+    num = int(a[i][1:])
+
+    if op == 'F':
+        x, y = move_to_waypoint(x, y, waypoint, num)
+    elif op in ['E', 'W', 'N', 'S']:
+        waypoint = move_waypoint(waypoint, op, num)
+    elif op in ['R', 'L']:
+        waypoint = rotate_waypoint(waypoint, op, num)
+    else:
+        print(f"don't know instruction {op} {num}")
+
+print(abs(x) + abs(y))
