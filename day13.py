68c26f35b961ab6df17bba0c414e6c56e3743a83
Nikola Kasev <nikola.kasev@gmail.com>
2020-12-13 12:14:38 +0100

    finds a cycle

--- day13.py
+++ day13.py
@@ -1,8 +1,9 @@
+from functools import lru_cache
+
 a = []
 for line in open('day13.txt').read().split('\n'):
     a.append(line)
 
-print(a)
 
 goal = int(a[0])
 departures = list(filter(lambda f: f != 'x', a[1].split(',')))
@@ -10,7 +11,47 @@ departures = list(filter(lambda f: f != 'x', a[1].split(',')))
 gaps = list(map(lambda d: (goal // int(d)) *
                 int(d) + int(d) - goal, departures))
 
-print(gaps)
 
 pairs = list(sorted(zip(gaps, departures)))
 print(int(pairs[0][0]) * int(pairs[0][1]))
+
+
+@lru_cache(maxsize=None)
+def f(t):
+    return t*17
+
+
+@lru_cache(maxsize=None)
+def g(t):
+    return t*13
+
+
+def find_start_cycle(f, g, delta):
+    t = 0
+    t2 = 0
+    start_of_cycle = False
+
+    while True:
+        tortoise = f(t)
+
+        t2 = 0
+        while True:
+            hare = g(t2)
+
+            if hare == (tortoise + delta):
+                start_of_cycle = True
+                break
+            elif hare > (tortoise + delta):
+                break
+            else:
+                t2 += 1
+
+        if start_of_cycle:
+            break
+        else:
+            t += 1
+
+    return(t, t2)
+
+
+print(find_start_cycle(f, g, 1))
