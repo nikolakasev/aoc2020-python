1f456404e4c9ae3f3f87f39792728e95423a3c09
Nikola Kasev <nikola.kasev@gmail.com>
2020-12-15 11:15:08 +0100

    solved D15P2

--- day15.py
+++ day15.py
@@ -1,44 +1,51 @@
-import sys
-sys.setrecursionlimit(2025)
-
-# input = [0, 3, 6]
-input = [6, 4, 12, 1, 20, 0, 16]
-
-memory = dict()
-
-
-for i in range(len(input)):
-    memory[input[i]] = i + 1
-
 
 def remember(memory, number, turn):
     if number in memory.keys():
         history = memory[number]
         if type(history) is tuple:
+            # the number was spoken twice already, shift turns - forget at history[0]
             memory[number] = (history[1], turn)
         else:
+            # the number was spoken once already, remember the second turn as well
             memory[number] = (history, turn)
     else:
+        # the first time the number was spoken
         memory[number] = turn
 
+    return memory
 
-def say(memory, turn, last_number_spoken, target):
-    history = memory[last_number_spoken]
 
+def say(memory, turn, number):
+    history = memory[number]
+
+    # for each spoken number, keep the last one (int) or last two (tuple) turns it was spoken
     if type(history) is tuple:
         speak = history[1] - history[0]
     elif type(history) is int:
         speak = 0
     else:
-        speak = last_number_spoken
+        speak = number
 
-    remember(memory, speak, turn)
-
-    if (target == turn):
-        return speak
-    else:
-        return say(memory, turn + 1, speak, target)
+    return speak
 
 
-print(say(memory, len(memory) + 1, 16, 2020))
-# print(8 in memory.keys())
+def play(numbers, target):
+    # prepare the memory with the numbers so far and their corresponding turns
+    m = dict()
+    for i in range(len(numbers)):
+        m[numbers[i]] = i + 1
+
+    number = numbers[-1]
+
+    for i in range(len(numbers) + 1, target + 1):
+        # which number to say?
+        number = say(m, i, number)
+        # make sure to remember it was said
+        m = remember(m, number, i)
+
+    return number
+
+
+n = [6, 4, 12, 1, 20, 0, 16]
+print(play(n, 2020))
+print(play(n, 30000000))
