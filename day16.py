7fb5e85a13776db36e89e7ee5ed3afdf0882bae6
Nikola Kasev <nikola.kasev@gmail.com>
2020-12-16 11:33:17 +0100

    map field positions to ranges

--- day16.py
+++ day16.py
@@ -5,30 +5,74 @@ range_pattern = r"(\w+\s?\w+):\s(\d+)-(\d+)\sor\s(\d+)-(\d+)"
 range_reg = re.compile(range_pattern)
 
 a = []
-for line in open('day16.txt').read().split('\n\n'):
+for line in open('s.txt').read().split('\n\n'):
     a.append(line)
 
-ranges = []
+valid_ranges = []
 for i in a[0].split("\n"):
-    ranges.append((int(range_reg.match(i).groups()[
-                  1]), int(range_reg.match(i).groups()[2])))
-    ranges.append((int(range_reg.match(i).groups()[
-                  3]), int(range_reg.match(i).groups()[4])))
+    valid_ranges.append((int(range_reg.match(i).groups()[
+        1]), int(range_reg.match(i).groups()[2])))
+    valid_ranges.append((int(range_reg.match(i).groups()[
+        3]), int(range_reg.match(i).groups()[4])))
 
 
-other_tickets = []
-for o in a[2].split("\n"):
-    if o != "nearby tickets:":
-        other_tickets.extend(o.split(","))
+def ticket_to_values(t):
+    return list(map(lambda f: int(f), t.split(",")))
 
+
+# add my ticket to the list of valid ones
+valid_tickets = [ticket_to_values(a[1].split("\n")[1])]
 
 invalid = 0
-for o in other_tickets:
-    valid = False
-    ticket = int(o)
+for ticket in a[2].split("\n"):
+    if ticket != "nearby tickets:":
+        valid = True
 
-    if sum(1 for i in ranges if ticket >= i[0] and ticket <= i[1]) == 0:
-        invalid += ticket
+        for f in ticket.split(","):
+            field = int(f)
+
+            # if the field value is not in at least onerange, the whole passport is invalid
+            if sum(1 for i in valid_ranges if field >= i[0] and field <= i[1]) == 0:
+                valid = False
+                invalid += field
+
+        if valid:
+            valid_tickets.append(ticket_to_values(ticket))
 
 
 print(invalid)
+print(valid_tickets)
+print(valid_ranges)
+
+# # for each field position
+# for i in range(len(valid_tickets[0])):
+#     # # for each ticket
+#     # for t in valid_tickets:
+#     #     # must fit exactly one field range
+#     #     if all(()):
+#     #         print(f"match found for {i}")
+#     for r in range(len(ranges) // 2):
+#         for t in valid_tickets:
+#             # print(f"looking at {t[i]}")
+#             if (t[i] >= ranges[r*2][0] and t[i] <= ranges[r*2][1]) \
+#                     or (t[i] >= ranges[r*2 + 1][0] and t[i] <= ranges[r*2 + 1][1]):
+#                 print(f"matched pos {i} for range {r}")
+#         # if all((True for t in valid_tickets if (t[i] >= ranges[r*2][0] and t[i] <= ranges[r*2][1])
+#         #         or (t[i] >= ranges[r*2 + 1][0] and t[i] <= ranges[r*2 + 1][1]))):
+#         #     print(
+#         #         f"match found for {i} in range {ranges[r*2]}, {ranges[r*2 + 1]}")
+
+
+def positions_to_ranges(tickets, positions, ranges, range_indexes):
+    return ((i, r) for i in positions for r in range_indexes
+            if len(tickets) == sum(1 for t in tickets
+                                   if (t[i] >= ranges[r*2][0] and t[i] <= ranges[r*2][1])
+                                   or (t[i] >= ranges[r*2 + 1][0] and t[i] <= ranges[r*2 + 1][1])))
+
+
+print(list(positions_to_ranges(valid_tickets, range(
+    len(valid_tickets[0])), valid_ranges, range(len(valid_ranges) // 2))))
+
+index_ranges = range(len(valid_ranges) // 2)
+
+print(list(index_ranges))
