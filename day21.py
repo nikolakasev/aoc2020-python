936300e42aec29070ae1865ce1696d20b9637841
Nikola Kasev <nikola.kasev@gmail.com>
2020-12-21 11:50:18 +0100

    soved D12P2, on paper

--- day21.py
+++ day21.py
@@ -15,7 +15,32 @@ ingredients = set(itertools.chain(*[f[0] for f in input]))
 
 potentials = set()
 for a in allergens:
+    print(
+        f"potentials for {a} are {set.intersection(*[set(f[0]) for f in input if a in f[1]])}")
+
     potentials.update(set.intersection(
         *[set(f[0]) for f in input if a in f[1]]))
 
+# star one
 print(sum(1 for f in input for i in f[0] if i in ingredients - potentials))
+
+# star two, solved by hand :)
+# potentials for sesame are {'lmxt', 'nmtzlj'}
+# potentials for peanuts are {'lmxt', 'gpxmf'}
+# potentials for soy are {'nmtzlj', 'dlkxsxg', 'gpxmf', 'fvqg'}
+# potentials for wheat are {'mxf', 'gpxmf', 'dxzq'}
+# potentials for dairy are {'lmxt'}
+# potentials for shellfish are {'dlkxsxg', 'gpxmf'}
+# potentials for fish are {'mxf', 'dlkxsxg'}
+# potentials for eggs are {'lmxt', 'dlkxsxg', 'fvqg', 'rggkbpj'}
+
+# lmxt dairy
+# rggkbpj eggs
+# mxf fish
+# gpxmf peanuts
+# nmtzlj sesame
+# dlkxsxg shellfish
+# fvqg soy
+# dxzq wheat
+
+# lmxt,rggkbpj,mxf,gpxmf,nmtzlj,dlkxsxg,fvqg,dxzq
